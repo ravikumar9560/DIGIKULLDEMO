1cc3ec71a4bc8baf2b0c6ce0962651a1cabeaebe
Ravi <163804993+ravikumar9560@users.noreply.github.com>
2024-06-30 17:17:11 +0530

    Add files via upload

---
+++ sum22.js
@@ -0,0 +1,72 @@
+
+let sum=0;
+for( let i=1; i<10;i++){
+    sum=sum+i;
+}
+console.log(sum);
+
+/*factor*/ 
+let num=20;
+for(let i=1;i<=num;i++){
+    if(num%i==0){   
+
+        console.log(i);
+    }
+}
+/**/ 
+
+
+function multiply(a,b)
+{
+    let multiply1=a*b;
+    console.log(multiply1);
+}
+multiply(78,78);
+multiply(4,87);
+multiply(1,87);
+multiply(2,87);
+/* odd and even*/
+
+for(let a=1; a<=20;a++){
+    if(a%2 !=0){
+        document.write(a+"<br>");
+    }
+}
+for(let i=1; i>=200;a++){
+
+    console.log(i);{
+        
+    }
+
+}
+
+
+
+
+//
+function isprime(n){
+    for( let i=2; i<n; i++)
+{
+    if(n%i===0){
+        return false
+    }
+}
+return true
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
