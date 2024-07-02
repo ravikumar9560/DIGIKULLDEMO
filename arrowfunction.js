bd5d750e292042beed61640389dd8945fba29e79
Ravi <163804993+ravikumar9560@users.noreply.github.com>
2024-07-02 11:42:28 +0530

    Add files via upload

---
+++ arrowfunction.js
@@ -0,0 +1,26 @@
+//Arrowfuncton
+var fullname=()=>{
+    console.log("ravi-yadav");
+
+}
+fullname()
+
+//sumoftheevennumber
+
+
+    //for(let i=0; i<10;i++){
+    
+    
+      //  console.log(i);
+    //}
+    //sumoftheevennumber
+    function sumofevennumber(p){
+    let sum=0;
+    for(let i=1;i<=10;i++){
+        if(i%==0){
+            sum=sum+i;
+        
+
+
+    } return sum;}
+    
