b40e43c0128d22c01b4c448dfc4f41b48fdff0e2
Ravi <163804993+ravikumar9560@users.noreply.github.com>
2024-07-13 18:48:20 +0530

    Add files via upload

---
+++ formtest.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+</head>
+
+<body>
+<table width="303" height="106" border="1" align="center">
+  <tr>
+    <td height="26"> Name : </td>
+	
+<td><input type="text" name  maxlength="100" placeholder="Name"></td></tr>
+  </tr>
+
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+<tr>
+    <td height="26"> Father's  : </td>
+	<td><input type="text" name  maxlength="100" placeholder="Father's" /></td>
+</tr>
+    
+  </tr>
+  
+  <tr>
+    <td height="30"> Father's Name : </td>
+    <td><input type="text"  Father's name="" /> </td>
+  </tr>
+</table>
+</body>
+</html>
