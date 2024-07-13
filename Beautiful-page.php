b5f45da5c3e4b40a835d4fbf0995047c2eb8e5ac
Ravi <163804993+ravikumar9560@users.noreply.github.com>
2024-07-13 18:49:35 +0530

    Add files via upload

---
+++ Beautiful-page.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Beautiful and Simple Website</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	
+</head>
+<body>
+	
+	
+		<div class="container">
+  <div class="row">
+    <div class="col-sm-1">
+      
+      <p>HOME..</p>
+    </div>
+    <div class="col-sm-1">
+      
+      <p>ABOUT..</p>
+    </div>
+    <div class="col-sm-1">
+      
+      <p>SERVICES..</p>
+    </div>
+    <div class="col-sm-1">
+      
+      <p>PORTFOLIO..</p>
+    </div>
+    <div class="col-sm-1">
+      
+      <p>PAGE..</p>
+    </div>
+    <div class="col-sm-1">
+      
+      <p>CONTACT..</p>
+    </div>
+    <div class="col-sm-4">
+      
+       <button type="button" class="btn-danger">GET FREE CONSULTANT</button>
+    </div>
+  </div>
+</div>
+<img src="images/slider/14.webp"  width="1700" height="800">
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      
+      <p >GET EVERY SINGLE SOLUTIONS.</p>
+    </div>
+  </div>
+  <div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      <p>I'm Desiner </p>
+      <div class="col-sm-6">
+        <p>Haris F Watson</p>
+      </div>
+      <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <p>jhorem rfpsum golor sidt amet. consecteture adipiscing elit elusmod tempor</p>
+    </div>
+      <div class="col-sm-4">
+        <p> <>
+      </div>
+        <div class="col-sm-4">
+        </div>
+	
+
+	
+
+</body>
+</html>
